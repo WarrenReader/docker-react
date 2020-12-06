68deac36a2bf6f8d7220e5a4ca551c2f30e9ca89
Warren Reader <warrendreader@gmail.com>
2020-12-06 13:55:31 -0700

    feat: updated app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there bud!!
+          James is the best!!
         </p>
         <a
           className="App-link"
